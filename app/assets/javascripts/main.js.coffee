ca1bce2b201802985a30c9c396e40a1b82036016
Eric Vass <ericrvass@gmail.com>
2014-06-27 10:06:40 -0400

    Disable button with loading message when submitted

--- app/assets/javascripts/main.js.coffee
+++ app/assets/javascripts/main.js.coffee
@@ -9,5 +9,13 @@ $(document).ready ->
     last_input.after new_input
     e.preventDefault()
     false
-
+  $("#submit_button").click (e) ->
+    $("#submit_button").button "loading"
+    $("#search_form").submit();
+    setTimeout (->
+      $("#submit_button").button  "reset"
+      return
+    ), 3000
+    e.preventDefault()
+    false
   return
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,5 @@
 require 'web_searcher'
+require 'uri'
 
 class MainController < ApplicationController
   def home
@@ -6,10 +7,15 @@ class MainController < ApplicationController
   end
   
   def search
-    html_items = params[:html_block]
-    @result = WebSearcher.find_html_in_site(html_items, params[:url])
-    respond_to do |format|
-      format.js
+    if !params[:html_block].blank? && !params[:url].blank? && params[:url] =~ URI::regexp
+      html_items = params[:html_block]
+      @result = WebSearcher.find_html_in_site(html_items, params[:url])
+      respond_to do |format|
+        format.js
+      end
+    else
+      #TODO: error handling
+      head :ok, :content_type => 'text/html'
     end
   end
 end
